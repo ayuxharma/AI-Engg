fd2a52411b730f89f29e1376547985792c21cc12
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2026-07-30 19:17:25 +0530

    hehe

--- FastAPI/PhotoVideoBackend/app/users.py
+++ FastAPI/PhotoVideoBackend/app/users.py
@@ -11,7 +11,7 @@ from fastapi_users.authentication import (
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User, get_user_db
 
-SECRET = "vyv5785v7d8gff7" #SECRET KEY 
+SECRET = "" #SECRET KEY 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]) :
     reset_password_token_secret = SECRET 
